0f2c8d512d60ae99ef089a4198089ed8eeb3601b
Łukasz Witt <witt.config@gmail.com>
2024-07-10 12:59:06 +0200

    kolejne

--- pages/userpanel1.tsx
+++ pages/userpanel1.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { FaCog, FaEnvelope, FaSearch, FaCalendarAlt } from 'react-icons/fa';
 import Useroption from '@/components/userpanel/nav/Useroption';
+import UserOptionWraper from '@/components/userpanel/nav/UserOptionWraper';
 
 
 const userpanel1 = () => {
   return (
     
 <>
- <Useroption/>
+ <UserOptionWraper/>
 
  
 </>
