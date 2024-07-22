ae6a1cd21a0fe88f2d9636455e8cb66c20640f54
Łukasz Witt <witt.config@gmail.com>
2024-07-22 08:44:16 +0200

    d

--- components/userpanel/content_components/Intro.tsx
+++ components/userpanel/content_components/Intro.tsx
@@ -1,47 +1,49 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
-import Button from '@mui/material/Button';
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
 import UpgradeIcon from '@mui/icons-material/Upgrade';
 
-
 function Intro() {
-  
-
-
-
-
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" padding="1rem" justifyContent="center">
-      <Typography variant="h2" gutterBottom fontWeight="bold"> Witaj!
-      </Typography>
-      <Typography variant="h2" gutterBottom fontWeight="bold"> Łukasz Witt
-      </Typography>
-     
-      <Box marginY= "8rem">
-    
-        <Typography marginY= "5rem" variant="h5" gutterBottom textAlign="center"> Nieprzeczytane wiadomości: 3
+    <Box  display="flex" flexDirection="column" alignItems="center" padding="1rem" justifyContent="center" height="100vh">
+      <Typography variant="h2" gutterBottom fontWeight="bold"> Witaj!</Typography>
+      <Typography variant="h2" gutterBottom fontWeight="bold"> Łukasz Witt</Typography>
+      <Box marginY="1rem">
+        <Typography marginY="5rem" variant="h5" gutterBottom textAlign="center">
+          Nieprzeczytane wiadomości: 3
         </Typography>
-
-        <Typography marginY= "2rem" variant="h5" gutterBottom textAlign="center">Aktualnie w ramach twojej subskrypcji pozostało Ci:
+        <Typography marginY="2rem" variant="h5" gutterBottom textAlign="center">
+          Aktualnie w ramach twojej subskrypcji pozostało Ci:
         </Typography>
-        
-        <Typography  variant="h5" gutterBottom textAlign="center">Wiadomości do wysłania:
+        <Typography variant="h5" gutterBottom textAlign="center">
+          Wiadomości do wysłania:
         </Typography>
-        
-        <Typography variant="h5" gutterBottom textAlign="center">Zaczepki :
+        <Typography variant="h5" gutterBottom textAlign="center">
+          Zaczepki :
         </Typography>
-        
-        <Typography variant="h5" gutterBottom textAlign="center">Zmiana lokalizacji:
+        <Typography variant="h5" gutterBottom textAlign="center">
+          Zmiana lokalizacji:
         </Typography>
-
-        <Button variant="contained" color="secondary" startIcon={<UpgradeIcon/>}>
-        Sprawdź co u nas się zmieniło!
-      </Button>       
+        <Box marginY="7rem" display="flex" justifyContent="center">
+          <Button className='ubuntu-medium'
+            variant="contained"
+            startIcon={<UpgradeIcon />}
+            sx={{
+              backgroundColor: '#A758B5',
+              borderColor: 'black',
+              border: 'solid',
+              '&:hover': {
+                backgroundColor: '#ffffff',
+                color: '#000000',
+              },
+              color: '#ffffff'
+            }}
+          >
+            Sprawdź co u nas się zmieniło!
+          </Button>
+        </Box>
       </Box>
     </Box>
-  )
+  );
 }
 
-export default Intro
-
-
+export default Intro;
