7103ae2f527f32b337a90192edb3aff123717cda
Łukasz Witt <witt.config@gmail.com>
2024-08-02 09:17:35 +0200

    theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,27 @@
+
+
+
 // import Background from "@/components/Background";
 import Swither from "@/components/Swither";
 import AuthContextProvider from "@/context/AuthContext";
 import RegisterContextProvider from "@/context/register/RegisterContext";
 import { SchoolsDataProvider } from "@/context/schoolsData/SchoolsDataProvider";
-import "@/styles/globals.css";
 import { Box } from "@mui/material";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "@mui/material/styles"; // Poprawiony import
+import maintheme from "@/theme/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SchoolsDataProvider>
       <AuthContextProvider>
         <RegisterContextProvider>
-          <Box sx={{ backgroundColor: "white", scrollBehavior: "smooth" }}>
-            <Component {...pageProps} />
-            <Swither />
-          </Box>
+          <ThemeProvider theme={maintheme}> {/* Dodano theme jako atrybut */}
+            <Box sx={{ backgroundColor: 'primary', scrollBehavior: "smooth" }}>
+              <Component {...pageProps} />
+              <Swither />
+            </Box>
+          </ThemeProvider>
         </RegisterContextProvider>
       </AuthContextProvider>
     </SchoolsDataProvider>
---
+++ theme/theme.ts
@@ -0,0 +1,33 @@
+import { createTheme } from '@mui/material/styles';
+import { PaletteOptions } from '@mui/material/styles/createPalette';
+
+// Rozszerz typy palety, aby uwzględniały niestandardowe kolory
+declare module '@mui/material/styles/createPalette' {
+  interface Palette {
+    custom: {
+      main: string;
+    };
+  }
+  interface PaletteOptions {
+    custom?: {
+      main: string;
+    };
+  }
+}
+
+const maintheme = createTheme({
+  palette: {
+    primary: {
+      main: '#A758B5',
+    },
+    secondary: {
+      main: '#FFFFFF',
+    },
+    
+    custom: {
+      main: '#000000',
+    },
+  },
+});
+
+export default maintheme;
